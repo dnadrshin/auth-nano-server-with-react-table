e43b780f332370f9c1d6aacf9566c294db315a37
dnadrshin <dimankaliningrad@gmail.com>
2017-10-16 20:10:50 +0200

    del console log

--- app/Table/Row/index.js
+++ app/Table/Row/index.js
@@ -5,7 +5,6 @@ import {convertFieldValue as converter} from '../../generic/helpers'
 const
     Row = ({data, columns, actions}) => <tr>
         {columns.map((column, i) => <td key={`${data._id}-col-${i}`}>
-            {console.log(actions, column.type, data._id)}
             {!column.isServiceField
                 ? converter(data[column.name], column.type)
 
