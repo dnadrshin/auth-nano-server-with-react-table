c17c9923b9206560e0f6466e61292f11b02e1c02
dnadrshin <dimankaliningrad@gmail.com>
2017-10-18 22:09:07 +0200

    user edit page

--- app/Users/Edit/index.js
+++ app/Users/Edit/index.js
@@ -2,52 +2,91 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Control, Form, Field } from 'react-redux-form';
-import { compose, withHandlers } from 'recompose';
-import SelectField from '../../generic/SelectField';
-import DateField from '../../generic/DateField';
+import { compose, withHandlers, lifecycle } from 'recompose';
+import SelectField from 'generic/SelectField';
+import DateField from 'generic/DateField';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
-import rest from '../rest'
+import rest from '../rest';
+import { track, actions } from 'react-redux-form';
+import {push} from 'react-router-redux';
+import Loading from 'generic/Loading'
 
 const
-    initForm = {email: '', date: moment(), distance: '', time: ''},
+    initForm = {username: '', firstName: '', date: moment(), lastName: '', password: '', role: ''},
 
-    UsersForm = (props: {
+    RecordForm = (props: {
         submit: Function,
-        record: {}
-    }) => <Form model="record" onSubmit={props.submit}>
-        <SelectField 
-            model="record.email"
-            listModel="users"
-            option="email"
-            value="id"
+        user: {}
+    }) => <Form model="user" onSubmit={props.submit}>
+        <Control.text model="user.username" id="user.email" placeholder="Email" />
+        <Control.text model="user.firstName" id="user.firstName" placeholder="First Name" />
+        <Control.text model="user.lastName" id="user.lastName" placeholder="Last Name" />
+        <Control.text model="user.password" id="user.password" placeholder="Password" />
+        
+        <SelectField
+            list={[
+                {value: 'user', option: 'User'},
+                {value: 'admin', option: 'Admin'},
+            ]}
+            model="user.role"
+            option="option"
+            value="value"
         />
-
-        <DateField
-            model="record.date"
-        />
-
-        <Control.text model="record.distance" id="record.distance" />
-        <Control.text model="record.time" id="record.time" />
         <button>Submit!</button>
+        {props.isLoading && <Loading />}
     </Form>;
 
 export default compose(
     connect(
         state => ({
-            record: state.record,
-            users : state.users,
+            isLoading: state.rest.user.loading,
+            user     : state.rest.user.data,
+            users    : state.users,
         }),
 
-        {
-            post: rest.actions.users.post,
-        }
+        (dispatch, props) => ({
+            sync   : (params, data, cb) => dispatch(rest.actions.user.get(params, data, cb)),
+            pushURL: url => dispatch(push(url)),
+            post   : (params, data, cb) => dispatch(rest.actions.user.post(params, data, cb)),
+            put    : (params, data, cb) => dispatch(rest.actions.user.put(params, data, cb)),
+            change : (model, value) => dispatch(actions.change(model, value)),
+            reset  : model => dispatch(actions.reset(model)),
+        })
     ),
 
+    lifecycle({
+        componentDidMount() {
+            if(this.props.params.id !== 'new')
+                this.props.sync(
+                    {id: this.props.params.id},
+                    null,
+
+                    (err, data) => {
+                        this.props.change('user.username', data.username);
+                        this.props.change('user.firstName', data.firstName);
+                        this.props.change('user.lastName', data.lastName);
+                        this.props.change('user.role', data.role);
+                    }
+                );
+        },
+        
+        componentWillUnmount() {
+            this.props.reset('user')
+        }
+    }),
+
     withHandlers({
-        submit: props => () => props.post()
+        submit: props => () => props[props.params.id === 'new' ? 'post' : 'put'](
+            props.params.id === 'new' ? null : {id: props.params.id},
+            {body: JSON.stringify({...props.user, token: localStorage.getItem('token')})},
+
+            (err, data)=> {
+                console.log(err, data);
+                props.pushURL('/users');
+        })
     })
-)(UsersForm);
+)(RecordForm);
 
 export {initForm};
