d640ad80e18da0a6a22d30e351e4a19db53fe11c
dnadrshin <dimankaliningrad@gmail.com>
2017-10-20 19:09:54 +0200

    add logger and fix formatting

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
 import {syncHistoryWithStore, routerMiddleware} from 'react-router-redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { Router, Route, browserHistory } from 'react-router';
+import {createLogger} from 'redux-logger';
+import {Router, Route, browserHistory} from 'react-router';
 import reducer from './reducer';
 import Users from './Users';
 import UsersEdit from './Users/Edit';
@@ -15,9 +15,8 @@ import RecordsEdit from './Records/Edit';
 import Header from './Header';
 import Registration from './Registration';
 
-const logger = createLogger({});
-
 const
+    logger = createLogger({}),
     store = createStore(reducer, applyMiddleware(thunk, logger, routerMiddleware(browserHistory)));
 
 console.log(store.getState());
