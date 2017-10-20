269c2d6846d0d12bf198bfa9ed69b6e941d1a8b6
dnadrshin <dimankaliningrad@gmail.com>
2017-10-20 19:06:56 +0200

    improve token checking

--- server/routes/API/middleware/token-check.js
+++ server/routes/API/middleware/token-check.js
@@ -2,12 +2,13 @@
 const
     jwt = require('jsonwebtoken'),
 
-    tokenCheck = (req, res, next) => {
-        jwt.verify(req.headers.token, 'tracker_key', (err, decoded) => {
-            if(err) return res.status(401);
+    tokenCheck = (req, res, next) => req.headers.token
+        ? jwt.verify(req.headers.token, 'tracker_key', (err, decoded) => {
+            if (err) return res.status(401);
             res.locals.decoded = decoded;
             next();
-        });
-    };
+        })
+
+        : res.status(401);
 
 module.exports = tokenCheck;
